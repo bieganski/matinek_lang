48bf02b3b7a9273196fafc6017370596139b486f
Mateusz <bieganski.m@wp.pl>
2019-06-09 10:18:00 +0200

    ADT processing semi-done

--- ADTProcessing.hs
+++ ADTProcessing.hs
@@ -3,6 +3,7 @@ module ADTProcessing where
 import Control.Monad.Reader
 import Control.Monad.Except
 import qualified Data.Map as Map
+import qualified Data.Set as Set
 
 import Types
 import ProgGrammar
@@ -13,16 +14,12 @@ type DataName = String
 
 type ValEnv = Map.Map VName Value
 type DataNameEnv = Map.Map ConstrName DataName
-type TypesEnv = Map.Map VName [Scheme] -- there also exists TypeEnv data,
+type TypesEnv = Map.Map VName Scheme -- there also exists TypeEnv data,
   -- here we use pure map for convenience
 
 type ADTEnv = (ValEnv, DataNameEnv, TypesEnv)
 
 type DeclProcess = ExceptT String (Reader ADTEnv)
-
--- Either a b -> ForAll [TV 'a', TV 'b'] (TADT "Either" ['a', 'b'])
-createADTScheme :: String -> [String] -> Scheme
-createADTScheme dname letters = generalize t0 $ TADT dname letters
 
 dataExists :: DataName -> ADTEnv -> Bool
 dataExists dname env = elem dname $ dFun Map.elems env
@@ -37,28 +34,22 @@ incArity (VADT _ _) = error "must be GHC error to that happen"
 incArity (VCon arity vadt) = VCon (arity + 1) vadt
 
 
--- parsing constructor we found next type (i.a 'b' in Either a b),
--- we enhance scheme associated with 'Either a'
-handleScheme :: TypesEnv -> Type -> [Scheme] -> [Scheme]
-handleScheme tenv t schs = (generalize tenv t):schs
-
-
 addConstrHelp :: ADTEnv -> Constr -> DataName -> [String] -> Scheme -> DeclProcess ADTEnv
 addConstrHelp e (Constr cname []) _ _  = e
 addConstrHelp e@(venv, denv, tenv) c@(Constr cname (t:ts)) dname letters sch = do
   addConstrHelp (venv', denv, tenv') where
     -- (!) :: Ord k => Map k a -> k -> a
     venv' = Map.insert cname (incArity (venv Map.! cname)) venv -- its like update
-    tenv' = Map.insert cname (handleScheme tenv t (tenv Map.! cname)) tenv
-    
-  
--- does not mess with types, only creates basic stuff
+
+
 addConstr :: ADTEnv -> Constr -> DataName -> [String] -> Scheme -> DeclProcess ADTEnv
-addConstr e@(venv, denv, tenv) c@(Constr cname _) dname letters sch =
+addConstr e@(venv, denv, tenv) c@(Constr cname types) dname letters sch = do
+  resSch = foldr joinTypeScheme sch (reverse types) -- TODO reverse?
+  validateJoined resSch
   addConstrHelp (venv', denv', tenv') c dname letters where
     venv' = Map.insert cname (VADT cname []) venv
     denv' = Map.insert cname dname
-    tenv' = Map.insert cname [sch]
+    tenv' = Map.insert cname resSch
     
 
 parseConstrs :: ADTEnv -> Decl -> Scheme -> DeclProcess ADTEnv
@@ -66,8 +57,18 @@ parseConstrs env (DataDecl dname letters []) _ = return env
 parseConstrs env (DataDecl dname letters (con:cons)) sch = do
   when (constrExists con env) throwError $ "Constructor name duplication! (" ++ (show con) ++ ")"
   addConstr env con letters sch
-  
-  
+
+
+joinTypeScheme :: Type -> Scheme -> Scheme
+joinTypeScheme t1 (Forall vars t2) = Forall vars (TArrr t1 t2)
+-- foldr joinTypeScheme sch types
+
+
+validateJoined :: Scheme -> DeclProcess ()
+validateJoined Forall vars t = do
+  let frees = Set.toList $ ftv t
+  when (any (flip notElem vars) frees) (throwError "type error: not declared type variable used")
+  return ()
 
 vFun :: (ValEnv -> a) -> ADTEnv -> a
 vFun f (v, _, _) = f v
@@ -78,6 +79,9 @@ dFun f (_, d, _) = f d
 tFun :: (TypesEnv -> a) -> ADTEnv -> a
 tFun f (_, _, t) = f t
 
+-- Either a b -> ForAll [TV 'a', TV 'b'] (TADT "Either" ['a', 'b'])
+createADTScheme :: String -> [String] -> Scheme
+createADTScheme dname letters = generalize t0 $ TADT dname letters
 
 newData :: Decl -> ADTEnv -> DeclProcess ADTEnv
 newData d@(DataDecl dname letters constrs) e@(venv, denv, tenv) = do
@@ -98,3 +102,17 @@ preprocessDataDecls env d@((DataDecl _ _ _):ds) = do
   newenv <- newData d env
   preprocessDataDecls newenv ds
 preprocessDataDecls env _:ds = preprocessDataDecls env ds
+
+
+
+
+handleSchemesMap :: [Scheme] -> Scheme
+
+
+type Env = (ValEnv, DataNameEnv)
+
+createEnv :: ADTEnv -> [Decl] -> DeclProcess (Env, TypeEnv)
+createEnv env, ds = do
+  (venv, denv, tenv) <- preprocessDataDecls env ds
+  tenv <- handleSchemesMap schemes
+  return ((venv, denv), tenv)
